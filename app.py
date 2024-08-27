a90a7d673f89d88671e6191edf92b6feec4e8506
Pinchai <>
2024-08-27 11:55:41 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message: ' + message)
+    send('Message received: ' + message, broadcast=True)
+
+
+if __name__ == '__main__':
+    app.run()
